e965bb34f6365956b6e35d178e6c73ef3f99d5a1
Luna Yao <40349250+ZnqbuZ@users.noreply.github.com>
2023-08-23 21:23:26 +0800

    Add cutAll, cutForSearch, suggestFreq, addWord.

--- src/lib.rs
+++ src/lib.rs
@@ -5,23 +5,22 @@ use std::sync::{OnceLock, RwLock};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
-}
+extern {}
 
 fn instance() -> &'static RwLock<jieba_rs::Jieba> {
     static INSTANCE: OnceLock<RwLock<jieba_rs::Jieba>> = OnceLock::new();
     INSTANCE.get_or_init(|| {
         let new_instance = RwLock::new(
             jieba_rs::Jieba::new());
-        
+
         let default_hans_dict = include_str!("dicts/default.hans.dict.txt");
         new_instance.write().unwrap()
             .load_dict(&mut BufReader::new(default_hans_dict.as_bytes())).unwrap();
-        
+
         let default_hant_dict = include_str!("dicts/default.hant.dict.txt");
         new_instance.write().unwrap()
             .load_dict(&mut BufReader::new(default_hant_dict.as_bytes())).unwrap();
-        
+
         new_instance
     })
 }
@@ -37,6 +36,39 @@ pub fn cut(text: &str, hmm: bool) -> Vec<JsValue> {
         .collect()
 }
 
+#[wasm_bindgen]
+#[allow(non_snake_case)]
+pub fn cutAll(text: &str) -> Vec<JsValue> {
+    instance().read().unwrap()
+        .cut_all(text)
+        .into_iter()
+        .map(JsValue::from)
+        .collect()
+}
+
+#[wasm_bindgen]
+#[allow(non_snake_case)]
+pub fn cutForSearch(text: &str, hmm: bool) -> Vec<JsValue> {
+    instance().read().unwrap()
+        .cut_for_search(text, hmm)
+        .into_iter()
+        .map(JsValue::from)
+        .collect()
+}
+
+#[wasm_bindgen]
+#[allow(non_snake_case)]
+pub fn suggestFreq(segment: &str) -> usize {
+    instance().write().unwrap()
+        .suggest_freq(segment)
+}
+
+#[wasm_bindgen]
+#[allow(non_snake_case)]
+pub fn addWord(word: &str, freq: Option<usize>, tag: Option<String>) -> usize {
+    instance().write().unwrap()
+        .add_word(word, freq, tag.as_deref())
+}
 
 #[wasm_bindgen]
 #[allow(non_snake_case)]
