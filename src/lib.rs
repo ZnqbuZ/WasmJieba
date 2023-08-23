6fadddbb34e3a41df9ba9fe7f5ae4a0d77cdaeaf
Luna Yao <me+github@zznq.buzz>
2023-08-23 12:57:22 +0800

    Add cut and loadDict.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,35 @@
 mod utils;
 
+use std::io::BufReader;
+use std::sync::{OnceLock, RwLock};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern {
-    fn alert(s: &str);
 }
 
+fn instance() -> &'static RwLock<jieba_rs::Jieba> {
+    static INSTANCE: OnceLock<RwLock<jieba_rs::Jieba>> = OnceLock::new();
+    INSTANCE.get_or_init(|| RwLock::new(
+        jieba_rs::Jieba::new()))
+}
+
+
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, WasmJieba!");
+#[allow(non_snake_case)]
+pub fn cut(text: &str, hmm: bool) -> Vec<JsValue> {
+    instance().read().unwrap()
+        .cut(text, hmm)
+        .into_iter()
+        .map(JsValue::from)
+        .collect()
+}
+
+
+#[wasm_bindgen]
+#[allow(non_snake_case)]
+pub fn loadDict(dict: &str) {
+    let mut reader = BufReader::new(dict.as_bytes());
+    instance().write().unwrap()
+        .load_dict(&mut reader).unwrap();
 }
