c870395afe42abc45738a3a4383421fc6348513e
Luna Yao <github@me.2n9.co>
2025-03-17 22:54:50 +0100

    Regenerate dicts.

---
+++ examples/build-dict/builder.py
@@ -0,0 +1,26 @@
+import os
+import shutil
+import pypinyin
+
+import jieba
+
+jieba.enable_paddle()
+# jieba.enable_parallel(12)
+
+print("Loading user dict...")
+jieba.load_userdict(r".\dict.txt.big")
+
+print("Building dicts...")
+shutil.copy(r".\dict.txt.big", r".\out\dict.txt.big")
+for dict in os.listdir("dicts"):
+    with open(f"dicts/{dict}", "r", encoding="utf-8") as fin:
+        print(dict)
+        new_dict = set([])
+        for line in fin:
+            for word in jieba.cut(line.strip(), use_paddle=True, HMM=False, cut_all=True):
+                if len(word) > 1:
+                    new_dict.add(word)
+    with open(f"out/{dict}", "w", encoding="utf-8") as fout:
+        new_dict = sorted(new_dict, key=pypinyin.lazy_pinyin)
+        for word in new_dict:
+            fout.write(word + "\n")
---
+++ tests/test.rs
@@ -0,0 +1,21 @@
+#[cfg(not(target_arch = "wasm32"))]
+#[test]
+fn cut() {
+    let test_sentences = [
+        "改革歷程",
+        "我们中出了一个叛徒",
+        "我来到北京清华大学",
+        "他来到了网易杭研大厦",
+        "小明硕士毕业于中国科学院计算所，后在日本京都大学深造",
+        "实变函数论与泛函分析",
+        "實變函數論與泛函分析",
+        "梅竹錦標對抗賽",
+        "小明畢業於國立交通大學資訊科學與工程研究所",
+        "新竹的交通大學要在2021年2月1日與台北的陽明大學合併",
+    ];
+    // wasmjieba::addWord("歷程", Some(1048576), Some("n".to_string()));
+    for sentence in test_sentences {
+        let result = wasmjieba::cut(sentence, true);
+        println!("{:?}", result);
+    }
+}
