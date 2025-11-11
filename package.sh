60b0be279998362f9cbc391a2cd6a7581d990985
Luna Yao <40349250+ZnqbuZ@users.noreply.github.com>
2025-11-11 19:33:09 +0100

    chore: simplify package.json

--- package.sh
+++ package.sh
@@ -9,7 +9,26 @@ cp ../package.dummy.json package.json
 cp ../package.nodejs.json nodejs/pkg/package.json
 cp ../package.web.json web/pkg/package.json
 
-VERSION=$(cargo pkgid | awk -F'[#@]' '{print $NF}')
+CARGO_TOML=../Cargo.toml
+VERSION=$(tomlq -r '.package.version' "$CARGO_TOML")
+AUTHORS=$(tomlq -r '.package.authors[]' "$CARGO_TOML" | jq -R . | jq -s .)
+DESCRIPTION=$(tomlq -r '.package.description' "$CARGO_TOML")
+REPOSITORY=$(tomlq -r '.package.repository' "$CARGO_TOML")
+LICENSE=$(tomlq -r '.package.license' "$CARGO_TOML")
+
 for MANIFEST in package.json nodejs/pkg/package.json web/pkg/package.json; do
   sed -i "s/__VERSION__/$VERSION/g" "$MANIFEST"
+  jq \
+    --argjson collaborators "$AUTHORS" \
+    --arg description "$DESCRIPTION" \
+    --arg repository "$REPOSITORY" \
+    --arg license "$LICENSE" \
+    -r \
+    '
+      .collaborators = $collaborators
+      | .description = $description
+      | .repository = {"type": "git", "url": $repository}
+      | .license = $license
+    ' "$MANIFEST" > $MANIFEST.tmp
+  mv $MANIFEST.tmp $MANIFEST
 done
